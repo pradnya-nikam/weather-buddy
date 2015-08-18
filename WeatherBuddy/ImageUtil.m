d7fee85769806ef9b5225c431bab2a22460ac50d
pradnya-ee <pnikam@equalexperts.com>
2015-08-18 17:28:22 +0530

    moved image fetching to background for fast scrolling

--- WeatherBuddy/ImageUtil.m
+++ WeatherBuddy/ImageUtil.m
@@ -11,13 +11,17 @@ NSString * const IMAGE_URL=@"http://openweathermap.org/img/w/%@.png";
 
 @implementation ImageUtil
 +(void)loadImageWithName:(NSString *)name inImageView:(UIImageView*)imageView {
-    dispatch_async(dispatch_get_main_queue(), ^{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+
         NSString *imageUrl = [NSString stringWithFormat:IMAGE_URL,name];
         
         NSData *imageFromNetwork = [NSData dataWithContentsOfURL:[NSURL URLWithString:imageUrl]];
-        
-        if(imageFromNetwork)
-            imageView.image = [UIImage imageWithData:imageFromNetwork];
+        if(imageFromNetwork){
+            dispatch_async(dispatch_get_main_queue(), ^{
+            
+                    imageView.image = [UIImage imageWithData:imageFromNetwork];
+            });
+        }
     });
 }
 @end
